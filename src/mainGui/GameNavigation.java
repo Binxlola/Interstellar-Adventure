d5046013c644edbeb6d0beb97786ceea9e49ae18
Jason Smit <jason.michael.smit@gmail.com>
2019-05-09 18:54:43 +1200

    Main nav buttons added.

--- src/mainGui/GameNavigation.java
+++ src/mainGui/GameNavigation.java
@@ -2,6 +2,8 @@ package mainGui;
 
 import javax.swing.JPanel;
 import javax.swing.JButton;
+import java.awt.FlowLayout;
+import javax.swing.ImageIcon;
 
 public class GameNavigation extends JPanel {
 
@@ -9,12 +11,34 @@ public class GameNavigation extends JPanel {
 	 * Create the panel.
 	 */
 	public GameNavigation() {
+		setBounds(0, 0, 1000, 600);
 		setLayout(null);
-		setBounds(0, 0, 994, 571);
 		
+		// ======Nav Buttons======
 		JButton travelPlanetBtn = new JButton("Travel to planet");
-		travelPlanetBtn.setBounds(0, 0, 140, 29);
+		travelPlanetBtn.setBounds(0, 0, 140, 30);
 		add(travelPlanetBtn);
+		
+		JButton travelOutpostBtn = new JButton("Travel to outpost");
+		travelOutpostBtn.setBounds(0, 30, 140, 30);
+		add(travelOutpostBtn);
+		
+		JButton newDayBtn = new JButton("Start a new day");
+		newDayBtn.setBounds(0, 60, 140, 30);
+		add(newDayBtn);
+		
+		JButton viewProgressBtn = new JButton("View progress");
+		viewProgressBtn.setBounds(0, 90, 140, 30);
+		add(viewProgressBtn);
+		
+		JButton viewShipBtn = new JButton("View ship");
+		viewShipBtn.setBounds(0, 120, 140, 30);
+		add(viewShipBtn);
+		
+		JButton inventoryBtn = new JButton("");
+		inventoryBtn.setIcon(new ImageIcon(GameNavigation.class.getResource("/images/inventory.png")));
+		inventoryBtn.setBounds(930, 0, 64, 64);
+		add(inventoryBtn);
 
 	}
 }
