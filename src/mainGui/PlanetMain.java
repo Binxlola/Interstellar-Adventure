25b935aba1a1160a13d6760130b953488f55a23c
chaosbib <chaosbib@CHARIEL>
2019-05-10 17:46:09 +1200

    + back btn working

--- src/mainGui/PlanetMain.java
+++ src/mainGui/PlanetMain.java
@@ -1,5 +1,8 @@
 package mainGui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
@@ -8,13 +11,22 @@ public class PlanetMain extends JPanel {
 	/**
 	 * Create the panel.
 	 */
-	public PlanetMain() {
+	public PlanetMain(MainScreen window) {
 		setBounds(0, 0, 1000, 600);
 		setLayout(null);
 		
-		JButton travelOutpostBtn = new JButton("This worked!");
-		travelOutpostBtn.setBounds(0, 30, 140, 30);
-		add(travelOutpostBtn);
+		JButton backBtn = new JButton("Back");
+		backBtn.setBounds(50, 450, 140, 30);
+		backBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				window.changeContent("mainScreen");
+			}
+		});
+		add(backBtn);
+		
+		JButton confirmBtn = new JButton("Confirm");
+		confirmBtn.setBounds(200, 450, 140, 30);
+		add(confirmBtn);
 	}
 
 }
