ad1ebe3c0b4530cca933c0e8e2a3c8762269420a
Jason Smit <jason.michael.smit@gmail.com>
2019-05-12 15:24:59 +1200

    Added JPanel for Inventory

---
+++ src/mainGui/InventoryContent.java
@@ -0,0 +1,15 @@
+package mainGui;
+
+import javax.swing.JPanel;
+
+public class InventoryContent extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public InventoryContent() {
+		setBounds(0, 0, 1000, 600);
+
+	}
+
+}
