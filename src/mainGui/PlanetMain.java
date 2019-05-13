f634cbaeb47834752bc088b30a08074a13fc549d
aev35 <aev35@uclive.ac.nz>
2019-05-13 21:49:15 +1200

    add checkbutton feature: at most 2 selected

--- src/mainGui/PlanetMain.java
+++ src/mainGui/PlanetMain.java
@@ -3,6 +3,7 @@ package mainGui;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,6 +11,9 @@ import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import crewManagement.Crew;
 import management.GameManager;
@@ -19,6 +23,7 @@ public class PlanetMain extends JPanel {
 	GameManager gameManager = GameManager.getInstance();
 	Crew crew = Crew.getInstance();
 	private List<JCheckBox> checkBoxList = new ArrayList<JCheckBox>();
+	private int maxSelected = 0;
 
 	/**
 	 * Create the panel.
@@ -64,9 +69,43 @@ public class PlanetMain extends JPanel {
 			this.add(crewTypeLbl);
 			
 			JCheckBox crewCheckBox = new JCheckBox("");
+			crewCheckBox.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					JCheckBox source = (JCheckBox) e.getSource();
+					if (source.isSelected()) {
+						maxSelected += 1;
+						if (maxSelected >= 2) {
+							disableCheckBoxes();
+						}
+					} else {
+						maxSelected -= 1;
+						enableCheckBoxes();
+					}
+				}
+			});
 			crewCheckBox.setBounds(300, y, 150, 20);
 			this.add(crewCheckBox);
 			checkBoxList.add(crewCheckBox);
+		}
+	}
+	
+	/**
+	 * Disables unselected check boxes
+	 */
+	private void disableCheckBoxes() {
+		for (JCheckBox checkBox: checkBoxList) {
+			if (!checkBox.isSelected()) {
+				checkBox.setEnabled(false);
+			}
+		}
+	}
+	
+	/**
+	 * Enables all check boxes
+	 */
+	private void enableCheckBoxes() {
+		for (JCheckBox checkBox: checkBoxList) {
+			checkBox.setEnabled(true);
 		}
 	}
 
