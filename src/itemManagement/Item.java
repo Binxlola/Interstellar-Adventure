a22de146e5d724b895fa6c57700fa6b7b737c015
aev35 <aev35@uclive.ac.nz>
2019-05-21 13:16:12 +1200

    add getName()

--- src/itemManagement/Item.java
+++ src/itemManagement/Item.java
@@ -5,6 +5,12 @@ import javax.swing.JButton;
 public interface Item {
 	
 	/**
+	 * Will return the name the item
+	 * @return A String representing the name of the item.
+	 */
+	public String getName();
+	
+	/**
 	 * Will return the category the item would fall into (the items type)
 	 * @return A String representing what kind of item this is.
 	 */
@@ -14,6 +20,18 @@ public interface Item {
 	 * Adds 1 to the items count, representing how many of this type item the player owns.
 	 */
 	public void addCount();
+	
+	/**
+	 * Gets the current number of item of this type
+	 * @return Current number of item of this type
+	 */
+	public int getCount();
+	
+	/**
+	 * Gets the chance of this item to drop
+	 * @return The chance of this item to drop
+	 */
+	public double getDropChance();
 	
 	/**
 	 * Creates a JButton that can be used to display the item in a players inventory.
