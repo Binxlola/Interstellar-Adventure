4404f1a41651af7268a7f887d6dd7bd0c77395b7
aev35 <aev35@uclive.ac.nz>
2019-05-21 15:17:31 +1200

    allow game to reset

--- src/management/GameManager.java
+++ src/management/GameManager.java
@@ -1,5 +1,6 @@
 package management;
 
+import crewManagement.Crew;
 import environment.Environment;
 
 /**
@@ -13,6 +14,11 @@ public class GameManager {
 	private int gameDuration;
 	private int crewSize;
 	private String shipName;
+	private int currentDay;
+	private int partsFound;
+	private int partsToFind;
+	private Crew crew = Crew.getInstance();
+
 	
 	/**
 	 * Private constructor, object can only be created from and inside call to the constructor.
@@ -40,6 +46,8 @@ public class GameManager {
 		this.setDuration(duration);
 		this.setCrewSize(size);
 		this.setShipName(name);
+		this.setPartsToFind(duration);
+		this.currentDay = 1;
 		Environment env = Environment.getInstance();
 		env.buildEnvironment(gameDuration);
 		System.out.println(this.gameDuration);
@@ -70,5 +78,103 @@ public class GameManager {
 	 */
 	private void setShipName(String name) {
 		this.shipName = name;
+	}
+	
+	/**
+	 * A method to calculate and set the number of ship parts to find, based on the players desired game length.
+	 * @param duration An Integer value describing the players desired game length.
+	 */
+	private void setPartsToFind(int duration) {
+		double temp = (2.0/3.0) * (double)duration;
+		int parts = (int)temp;
+		partsToFind = parts;
+		
+	}
+	
+	public void startNewDay() {
+		// Move to new day if possible.
+		if(!this.endGame()) {
+			this.currentDay += 1;
+			crew.resetCrewMoves(); // reset all crew member move counts.
+			// RANDOM OCCURENCE
+			//alienPirates.randomAttack();
+		}
+		else {
+			System.out.println("You have reached the end of your " + this.gameDuration + " day journey.");
+			
+			if(this.allPartsFound()) {
+				System.out.println("You have found all the missing ship parts, you need to install them to win the game.");
+			}
+			else {
+				// Need to run a check to see if the player could possibly still find and install the missing parts............
+				System.out.println("You are still missing " + this.partsToFind + " Ship Parts, find and install them to win the game." );
+			}
+			// NOT COMPLETE
+		}
+	}
+	
+	/**
+	 * Adds 1 to the partsFound and subtracts 1 to the partsToFind
+	 */
+	public void partFound() {
+		partsToFind -= 1;
+		partsFound +=1;
+	}
+	
+	/**
+	 * Checks if the player already found all the parts required
+	 * @return Returns true if the player already found all the parts required
+	 */
+	public boolean allPartsFound() {
+		if(this.partsFound == this.partsToFind) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	/**
+	 * Get the current day of the game
+	 * @return Current day of the game
+	 */
+	public int getCurrentDay() {
+		return this.currentDay;
+	}
+	
+	/**
+	 * Get the duration of the game
+	 * @return The duration of the game
+	 */
+	public int getGameDuration() {
+		return this.gameDuration;
+	}
+	
+	/**
+	 * Checks if the game already reached the last day
+	 * @return True if the game reached the last day
+	 */
+	public boolean endGame() {
+		if (this.currentDay <= this.gameDuration) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	/**
+	 * Gets the remaining ship parts to find
+	 * @return The number of ship parts to find
+	 */
+	public int getPartsToFind() {
+		return this.partsToFind;
+	}
+	
+	/**
+	 * Gets the current number of ship parts found
+	 * @return The number of ship parts already found
+	 */
+	public int getPartsFound() {
+		return this.partsFound;
 	}
 }
